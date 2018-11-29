074737ac46aa6c0a70ab8453082c2f0a2463255f
Sven Dowideit <SvenDowideit@home.org.au>
2018-11-29 22:01:04 +1000

    work more with the sparkfun rfid reader
    Signed-off-by: Sven Dowideit <SvenDowideit@home.org.au>

--- ThingMagic/main.py
+++ ThingMagic/main.py
@@ -17,11 +17,13 @@ import sys
 sys.path.append('./mqtt/')
 import mqtt
 import config
+import datetime
 
 mqttHost = config.getValue("mqtthostname", "mqtt")
 myHostname = config.getValue("hostname", socket.gethostname())
 hostmqtt = mqtt.MQTT(mqttHost, myHostname, "ThingMagic")
 
+# see https://github.com/gotthardp/python-mercuryapi
 import mercury
 
 # end load config
@@ -30,12 +32,13 @@ import mercury
 
 def rfidTagDataCallback(rfid):
     try:
-        hostmqtt.publish("scan", {
-                'atr': rfid.epc_mem_data,
-                'tag': rfid.epc.hex(),
-                'rssi': rfid.rssi,
-                'event': 'inserted'
-            })
+        event = {
+            'atr': rfid.epc_mem_data,
+            'tag': rfid.epc.hex(),
+            'rssi': rfid.rssi,
+            'event': 'inserted'
+        }
+        hostmqtt.publish("scan", event)
 
         print("EPC: %s RSSI: %s\n" % (rfid.epc, rfid.rssi))
         print("     epc_mem_data: %s" % rfid.epc_mem_data)
@@ -47,20 +50,36 @@ def rfidTagDataCallback(rfid):
 
 
 ########################################
+def status():
+    hostmqtt.status({
+        "model": reader.get_model(),
+        "region": "AU",
+        #"temp": reader.get_temperature(),
+        "power_range": reader.get_power_range(),
+        "antennas": reader.get_antennas(),
+#        "power": reader.get_read_powers(),
+        "status": "listening"})
+
+########################################
 
 reader = mercury.Reader("tmr:///dev/ttyACM0")
 
 reader.set_region("AU")
 reader.set_read_plan([1], "GEN2")
+reader.set_read_powers([1], [2700])
 
+#using this causes the python to hang
+#reader.set_read_plan([1], "GEN2", bank=["reserved"], read_power=2700)
 
-print(reader.get_model())
-print(reader.get_antennas())
-print(reader.get_read_powers())
-
-hostmqtt.status({"status": "listening"})
+lastStatus = datetime.datetime.now()
+status()
 reader.start_reading(rfidTagDataCallback, on_time=100, off_time=0)
+
+
 while True:
+    if datetime.timedelta.total_seconds(datetime.datetime.now()-lastStatus) > (2*60):
+        status()
+        lastStatus = datetime.datetime.now()
     try:
         time.sleep(1)
         print(".")
