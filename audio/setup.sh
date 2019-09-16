28b3879530dc9afadc6013af97fa029c7c578e14
Sven Dowideit <SvenDowideit@home.org.au>
2019-09-16 22:33:26 +1000

    fixes for using pyo
    Signed-off-by: Sven Dowideit <SvenDowideit@home.org.au>

--- audio/setup.sh
+++ audio/setup.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # used to install scratch to get lots of wav files, but its 400MB
-sudo apt-get install -yq python3-pygame
+sudo DEBIAN_FRONTEND=noninteractive apt-get install -yq python3-pyo
 
 sudo pip3 install --no-cache-dir -r ./audio/requirements.txt
 
